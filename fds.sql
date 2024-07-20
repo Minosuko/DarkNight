2e5f5964075d228ce1690914ee546cc68e6913d1
Minosuko <63941519+Minosuko@users.noreply.github.com>
2024-07-20 16:31:35 +0700

    Add files via upload

---
+++ fds.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2024 at 11:31 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `comment_time` int(11) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friendship`
+--
+
+CREATE TABLE IF NOT EXISTS `friendship` (
+  `user1_id` int(11) NOT NULL,
+  `user2_id` int(11) NOT NULL,
+  `friendship_status` int(11) NOT NULL,
+  KEY `user1_id` (`user1_id`),
+  KEY `user2_id` (`user2_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE IF NOT EXISTS `likes` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE IF NOT EXISTS `media` (
+  `media_id` int(11) NOT NULL AUTO_INCREMENT,
+  `media_hash` varchar(255) NOT NULL,
+  `media_format` varchar(255) NOT NULL,
+  `media_ext` varchar(255) NOT NULL,
+  PRIMARY KEY (`media_id`),
+  UNIQUE KEY `media_hash` (`media_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(1) NOT NULL,
+  `from_id` int(11) NOT NULL,
+  `notification_time` int(11) NOT NULL,
+  `link_to` varchar(255) NOT NULL,
+  PRIMARY KEY (`notification_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_caption` text NOT NULL,
+  `post_time` int(11) NOT NULL,
+  `post_public` char(1) NOT NULL,
+  `post_by` int(11) NOT NULL,
+  `post_media` int(11) NOT NULL DEFAULT 0,
+  `is_share` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`post_id`),
+  KEY `post_by` (`post_by`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_token` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `session_device` varchar(255) NOT NULL,
+  `session_ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` int(1) NOT NULL DEFAULT 0,
+  `user_firstname` varchar(20) NOT NULL,
+  `user_lastname` varchar(20) NOT NULL,
+  `user_nickname` varchar(20) DEFAULT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_gender` char(1) NOT NULL,
+  `user_birthdate` int(11) NOT NULL,
+  `user_create_date` int(11) NOT NULL,
+  `user_status` char(1) DEFAULT NULL,
+  `user_about` text DEFAULT NULL,
+  `user_hometown` varchar(255) DEFAULT NULL,
+  `user_token` varchar(255) NOT NULL,
+  `pfp_media_id` int(11) NOT NULL,
+  `cover_media_id` int(11) NOT NULL,
+  `verified` int(1) NOT NULL DEFAULT 0,
+  `online_status` int(1) NOT NULL DEFAULT 1,
+  `last_online` int(11) NOT NULL DEFAULT 0,
+  `last_username_change` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`),
+  UNIQUE KEY `user_nickname` (`user_nickname`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_phone`
+--
+
+CREATE TABLE IF NOT EXISTS `user_phone` (
+  `user_id` int(11) DEFAULT NULL,
+  `user_phone` int(11) DEFAULT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
