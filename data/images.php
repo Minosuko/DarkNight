5a4e97608b81145e04a2d6ccb10f7a294fbdaefb
Minosuko <63941519+Minosuko@users.noreply.github.com>
2024-07-24 18:24:05 +0700

    remove unused/useless code

--- data/images.php
+++ data/images.php
@@ -1,22 +1,19 @@
 <?php
-if (!isset($_COOKIE['token']))
-    header("location:index.php");
 require_once '../includes/functions.php';
-if (!_is_session_valid($_COOKIE['token']))
-    header("location:/index.php");
 $compressed = true;
 if(isset($_GET['original']))
 	$compressed = false;
-if(isset($_GET['h'])){
-	if(file_exists("images/image/{$_GET['h']}.bin") || file_exists("images/compressed/{$_GET['h']}.bin")){
-		if($compressed && !file_exists("images/compressed/{$_GET['h']}.bin"))
-			file_put_contents("images/compressed/{$_GET['h']}.bin",compress_image("images/image/{$_GET['h']}.bin"));
-		if(file_exists("images/compressed/{$_GET['h']}.bin") && $compressed){
-			$fdate = filemtime("images/compressed/{$_GET['h']}.bin");
-			$etag = md5_file("images/compressed/{$_GET['h']}.bin");
+if(isset($_GET['id']) && isset($_GET['h'])){
+	$hash = _trim_hash($_GET['h']);
+	if(file_exists("images/image/$hash.bin") || file_exists("images/compressed/$hash.bin")){
+		if($compressed && !file_exists("images/compressed/$hash.bin"))
+			file_put_contents("images/compressed/$hash.bin",compress_image("images/image/$hash.bin"));
+		if(file_exists("images/compressed/$hash.bin") && $compressed){
+			$fdate = filemtime("images/compressed/$hash.bin");
+			$etag = md5_file("images/compressed/$hash.bin");
 		}else{
-			$fdate = filemtime("images/image/{$_GET['h']}.bin");
-			$etag = md5_file("images/image/{$_GET['h']}.bin");
+			$fdate = filemtime("images/image/$hash.bin");
+			$etag = md5_file("images/image/$hash.bin");
 		}
 	}else{
 		$fdate = time();
@@ -37,7 +34,7 @@ $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_
 $if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
 if ((($if_none_match && $if_none_match == $etag) || (!$if_none_match)) && ($if_modified_since && $if_modified_since == $tsstring))
 {
-    header('HTTP/1.1 304 Not Modified');
+	header('HTTP/1.1 304 Not Modified');
 	exit();
 }else{
 	if(isset($_GET['t'])){
@@ -49,73 +46,44 @@ if ((($if_none_match && $if_none_match == $etag) || (!$if_none_match)) && ($if_m
 			header("Last-Modified: $tsstring");
 			header("ETag: $md5");
 			readfile("images/M.jpg");
-		}
-		if($type == "default_F"){
+		}elseif($type == "default_F"){
 			$md5 = md5_file("images/F.jpg");
 			header('Content-Disposition: filename="Default_F.jpg"');
 			header("Content-Type: image/jpeg");
 			header("Last-Modified: $tsstring");
 			header("ETag: $md5");
 			readfile("images/F.jpg");
+		}else{
+			if(isset($_GET['id']) && isset($_GET['h']){
+				if(is_numeric($_GET['id'])){
+					if(file_exists("images/image/$hash.bin") || file_exists("images/compressed/$hash.bin")){
+						$md5 = $_GET['h'];
+						$query = $conn->query(
+							sprintf(
+								"SELECT * FROM media WHERE media_hash = '%s' AND media_id = %d",
+								$conn->real_escape_string($md5),
+								$conn->real_escape_string($_GET['id'])
+							)
+						);
+						if($query->num_rows > 0){
+							$fetch = $query->fetch_assoc();
+							if($compressed){
+								$fetch['media_ext'] = "jpg";
+								$fetch['media_format'] = "image/jpeg";
+							}
+							header('Content-Disposition: filename="'.$md5.'.'.$fetch['media_ext'].'"');
+							header("Content-Type: {$fetch['media_format']}");
+							header("Last-Modified: $tsstring");
+							header("ETag: $md5");
+							if($compressed)
+								readfile("images/compressed/$md5.bin");
+							else
+								readfile("images/image/$md5.bin");
+						}
+					}
+				}
+			}
 		}
-		if($type == 'profile')
-			if(isset($_GET['id']))
-				if(is_numeric($_GET['id']))
-					if(isset($_GET['h']))
-						if(file_exists("images/image/{$_GET['h']}.bin") || file_exists("images/compressed/{$_GET['h']}.bin")){
-							$md5 = $_GET['h'];
-							$query = $conn->query(
-								sprintf(
-									"SELECT * FROM media WHERE media_hash = '%s' AND media_id = %d",
-									$conn->real_escape_string($md5),
-									$conn->real_escape_string($_GET['id'])
-								)
-							);
-							if($query->num_rows > 0){
-								$fetch = $query->fetch_assoc();
-								if($compressed){
-									$fetch['media_ext'] = "jpg";
-									$fetch['media_format'] = "image/jpeg";
-								}
-								header('Content-Disposition: filename="'.$md5.'.'.$fetch['media_ext'].'"');
-								header("Content-Type: {$fetch['media_format']}");
-								header("Last-Modified: $tsstring");
-								header("ETag: $md5");
-								if($compressed)
-									readfile("images/compressed/$md5.bin");
-								else
-									readfile("images/image/$md5.bin");
-							}
-						}
-		if($type == "media")
-			if(isset($_GET['id']))
-				if(is_numeric($_GET['id']))
-					if(isset($_GET['h']))
-						if(file_exists("images/image/{$_GET['h']}.bin") || file_exists("images/compressed/{$_GET['h']}.bin")){
-							$md5 = $_GET['h'];
-							$query = $conn->query(
-								sprintf(
-									"SELECT * FROM media WHERE media_hash = '%s' AND media_id = %d",
-									$conn->real_escape_string($md5),
-									$conn->real_escape_string($_GET['id'])
-								)
-							);
-							if($query->num_rows > 0){
-								$fetch = $query->fetch_assoc();
-								if($compressed){
-									$fetch['media_ext'] = "jpg";
-									$fetch['media_format'] = "image/jpeg";
-								}
-								header('Content-Disposition: filename="'.$md5.'.'.$fetch['media_ext'].'"');
-								header("Content-Type: {$fetch['media_format']}");
-								header("Last-Modified: $tsstring");
-								header("ETag: $md5");
-								if($compressed)
-									readfile("images/compressed/$md5.bin");
-								else
-									readfile("images/image/$md5.bin");
-							}
-						}
 	}
 }
 ?>
