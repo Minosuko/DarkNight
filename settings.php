b5c58b17c9e673e5e95472c34cd522e21206eeb5
Minosuko <63941519+Minosuko@users.noreply.github.com>
2024-07-28 12:04:26 +0700

    Kofi

--- settings.php
+++ settings.php
@@ -155,6 +155,11 @@ $data = _get_data_from_token($_COOKIE['token']);
 									</a>
 								</div>
 							</div>
+							<div class="setting__box">
+								<div class="index_input_box xsetting asetting">
+									<a href='https://ko-fi.com/X8X8CYLKP' target='_blank' linked="true"><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+								</div>
+							</div>
 						</div>
 					</center>
 				</div>
