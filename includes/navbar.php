27cef81469c286d5c6d7287ce4426e1532cd3a52
Minosuko <63941519+Minosuko@users.noreply.github.com>
2024-07-29 10:28:35 +0700

    Rework

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,7 +6,7 @@ $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_
 $if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
 if ((($if_none_match && $if_none_match == $md5) || (!$if_none_match)) && ($if_modified_since && $if_modified_since == $tsstring))
 {
-    header('HTTP/1.1 304 Not Modified');
+	header('HTTP/1.1 304 Not Modified');
 }else{
 	header("Last-Modified: $tsstring");
 	header("ETag: $md5");
@@ -14,43 +14,44 @@ if ((($if_none_match && $if_none_match == $md5) || (!$if_none_match)) && ($if_mo
 if(!isset($IFI)) die("");
 ?>
 <script src="resources/js/jquery.js"></script>
-			<script src="resources/js/highlight.js"></script>
-			<div id="hljs_lang_list"></div>
-			<div class="usernav">
-				<center>
-					<ul>
-						<li><a href="/search.php" title="Settings"><i class="fa-solid fa-magnifying-glass"></i></a></li>
-						<li><a href="/requests.php" title="Friend Requests" class="noft-align"><i class="fa-solid fa-user-plus"></i><span class="red-dot-counter" id="friend_req_count">0</span></a></li>
-						<li><a href="/friends.php" title="Friends"><i class="fa-solid fa-user-group"></i></a></li>
-						<li><a href="/home.php" title="Home"><i class="fa-solid fa-house"></i></a></li>
-						<li><a href="/profile.php" title="Profile"><i class="fa-solid fa-user"></i></a></li>
-						<li><a href="/notification.php" title="Notification" class="noft-align"><i class="fa-solid fa-bell"></i><span class="red-dot-counter" id="notification_count">0</span></a></li>
-						<li><a href="/settings.php" title="Settings"><i class="fa-solid fa-gear"></i></a></li>
-					</ul>
-				</center>
-			</div>
-			<script src="resources/js/zTemplate.js"></script>
-			<script src="resources/js/main.js"></script>
-			<script src="resources/js/cropper.js"></script>
-			<script src="resources/js/jquery-cropper.js"></script>
-			<script>
+		<script src="resources/js/highlight.js"></script>
+		<div id="hljs_lang_list"></div>
+		<div class="usernav">
+			<center>
+				<div id="loading_bar"></div>
+				<ul>
+					<li><a href="/search.php" title="Settings"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+					<li><a href="/requests.php" title="Friend Requests" class="noft-align"><i class="fa-solid fa-user-plus"></i><span class="red-dot-counter" id="friend_req_count">0</span></a></li>
+					<li><a href="/friends.php" title="Friends"><i class="fa-solid fa-user-group"></i></a></li>
+					<li><a href="/home.php" title="Home"><i class="fa-solid fa-house"></i></a></li>
+					<li><a href="/profile.php" title="Profile"><i class="fa-solid fa-user"></i></a></li>
+					<li><a href="/notification.php" title="Notification" class="noft-align"><i class="fa-solid fa-bell"></i><span class="red-dot-counter" id="notification_count">0</span></a></li>
+					<li><a href="/settings.php" title="Settings"><i class="fa-solid fa-gear"></i></a></li>
+				</ul>
+			</center>
+		</div>
+		<script src="resources/js/zTemplate.js"></script>
+		<script src="resources/js/main.js"></script>
+		<script src="resources/js/cropper.js"></script>
+		<script src="resources/js/jquery-cropper.js"></script>
+		<script>
 			function validateField(){
-			    var query = document.getElementById("query");
-			    var button = document.getElementById("querybutton");
-			    if(query.value == "") {
-			        query.placeholder = 'Type something!';
-			        return false;
-			    }
-			    return true;
+				var query = document.getElementById("query");
+				var button = document.getElementById("querybutton");
+				if(query.value == "") {
+					query.placeholder = 'Type something!';
+					return false;
+				}
+				return true;
 			}
-			</script>
-			<?php
-			echo "<input id='fullname' value='".htmlspecialchars($data['user_firstname'] . ' ' . $data['user_lastname'])."' type='hidden'>";
-			echo "<input id='online_status' value='".$data['online_status']."' type='hidden'>";
-			?>
-			<div id="modal" class="modal">
-				<div class="modal-content" id="modal-content">
-					<a class="close" onclick="modal_close()">&times;</a>
-					<div id="modal_content"></div>
-				</div>
+		</script>
+		<?php
+		echo "<input id='fullname' value='".htmlspecialchars($data['user_firstname'] . ' ' . $data['user_lastname'])."' type='hidden'>\n";
+		echo "		<input id='online_status' value='".$data['online_status']."' type='hidden'>";
+		?>
+		<div id="modal" class="modal">
+			<div class="modal-content" id="modal-content">
+				<a class="close" onclick="modal_close()">&times;</a>
+				<div id="modal_content"></div>
 			</div>
+		</div>
