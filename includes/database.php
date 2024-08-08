49de839ce39b015d6e7852a7a656701c9cee2ddd
Minosuko <Catover35390@gmail.com>
2024-08-08 23:00:41 +0700

    Move

--- includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$IFI = 1;
-// Establish Connection to Database
-$host = 'localhost';//database host
-$username = 'root';//database user name
-$dbpassword = '';//database password
-$dbdata = 'FDS';//database
-?>
