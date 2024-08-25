8da044cc1c00d2dce0a94dac84c53bb7fd62c6f9
Minosuko <Catover35390@gmail.com>
2024-08-25 14:09:22 +0700

    Add Gmail

--- settings.php
+++ settings.php
@@ -156,6 +156,16 @@
 							</div>
 							<div class="setting__box">
 								<div class="index_input_box xsetting asetting">
+									<a href="mailto:dev3.darknight@gmail.com" target="_blank" linked="true">
+										<p>Gmail <i class="fa-solid fa-cloud"></i></p>
+										<div class="psetting">
+											<i class="fa-solid fa-angle-right"></i>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="setting__box">
+								<div class="index_input_box xsetting asetting">
 									<a href='https://ko-fi.com/X8X8CYLKP' target='_blank' linked="true"><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
 								</div>
 							</div>
