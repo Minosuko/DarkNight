cfd47ca43ad17a589e89f676640c1f75fbb2900d
Minosuko <63941519+Minosuko@users.noreply.github.com>
2024-08-27 16:19:01 +0700

    update

--- data/captcha.php
+++ data/captcha.php
@@ -2,6 +2,5 @@
 session_start();
 require_once("../includes/Captcha.php");
 $Captcha = new Captcha();
-$code = (isset($_SESSION['refresh_captcha']) && isset($_SESSION['captcha_code'])) ? ($_SESSION['refresh_captcha'] == 1 ? $Captcha->newCode() : $_SESSION['captcha_code']) : $Captcha->newCode();
-$Captcha->phpcaptcha('#ff6600','#fff',120,40,10,25,'#0066ff', $code);     
+$Captcha->phpcaptcha('#ff6600','#fff',120,40,10,25,'#0066ff', ((isset($_SESSION['refresh_captcha']) && isset($_SESSION['captcha_code'])) ? ($_SESSION['refresh_captcha'] == 1 ? $Captcha->newCode() : $_SESSION['captcha_code']) : $Captcha->newCode()));     
 ?>
