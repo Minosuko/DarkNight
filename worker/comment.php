6f6bc804cc6a40f766a7c93fc72d25cc7a37be5a
Minosuko <Catover35390@gmail.com>
2024-08-30 14:19:09 +0700

    add command

--- worker/comment.php
+++ worker/comment.php
@@ -9,16 +9,29 @@ if(isset($_GET['id'])){
 			$comment = $_POST['comment'];
 			$post_id = $_GET['id'];
 			$user_id = $data['user_id'];
+			if(!is_post_exists($post_id)) die('{"success":-1}');
+			$postinfo = getInfoPostID($post_id);
+			if($postinfo['allow_comment'] == 0) die('{"success":-1}');
+			$cf = $GLOBALS['commandfunc'];
+			$cf->setUserData($data);
+			$isCommand = $cf->parse_command();
+			if($isCommand){
+				switch(strtolower($isCommand[0])){
+					case 'verify':
+						$cf->execute($isCommand[0],$isCommand[1],$postinfo['post_by']);
+						break;
+					case 'allow_comment':
+						$cf->execute($isCommand[0],$isCommand[1],$post_id);
+						break;
+				}
+				die('{"success":1}');
+			}
 			$sql = sprintf(
 				"INSERT INTO `comments` (`post_id`, `user_id`, `comment`, `comment_time`) VALUES ('$post_id', '$user_id', '$comment', '$timestamp')",
 				$conn->real_escape_string($comment)
 			);
 			$query = $conn->query($sql);
 			if($query){
-				//if (isset($_FILES['fileUpload'])) {
-				//	$last_id = $conn->insert_id;
-				//	include '../functions/upload.php';
-				//}
 				echo '{"success":1}';
 			}
 		}
