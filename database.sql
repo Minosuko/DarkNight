bf93cb44bce58a5710206cc9dd4ccdb1234d9cf8
Minosuko <Catover35390@gmail.com>
2024-08-30 10:42:50 +0700

    Update charset

--- database.sql
+++ database.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `comment` text NOT NULL,
+  `comment` text COLLATE utf8mb4_bin NOT NULL,
   `comment_media` int(11) NOT NULL,
   `comment_time` int(11) NOT NULL,
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `follows` (
   `user1_id` int(11) NOT NULL,
   `user2_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `friendship` (
   `user1_id` int(11) NOT NULL,
@@ -23,77 +23,78 @@ CREATE TABLE IF NOT EXISTS `friendship` (
   `friendship_status` int(11) NOT NULL,
   KEY `user1_id` (`user1_id`),
   KEY `user2_id` (`user2_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `likes` (
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `media` (
   `media_id` int(11) NOT NULL AUTO_INCREMENT,
-  `media_hash` varchar(255) NOT NULL,
-  `media_format` varchar(255) NOT NULL,
-  `media_ext` varchar(255) NOT NULL,
+  `media_hash` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `media_format` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `media_ext` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`media_id`),
   UNIQUE KEY `media_hash` (`media_hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `notification` (
   `notification_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(1) NOT NULL,
   `from_id` int(11) NOT NULL,
   `notification_time` int(11) NOT NULL,
-  `link_to` varchar(255) NOT NULL,
+  `link_to` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`notification_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `posts` (
   `post_id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_caption` text NOT NULL,
+  `post_caption` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `post_time` int(11) NOT NULL,
-  `post_public` char(1) NOT NULL,
+  `post_public` char(1) CHARACTER SET utf8 NOT NULL,
   `post_by` int(11) NOT NULL,
   `post_media` int(11) NOT NULL DEFAULT 0,
   `is_share` int(11) NOT NULL DEFAULT 0,
+  `allow_comment` int(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`post_id`),
   KEY `post_by` (`post_by`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `session` (
-  `session_id` varchar(32) NOT NULL,
-  `session_token` varchar(255) NOT NULL,
+  `session_id` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `session_token` varchar(255) CHARACTER SET utf8 NOT NULL,
   `user_id` int(11) NOT NULL,
-  `session_device` varchar(255) NOT NULL,
-  `session_ip` varchar(255) NOT NULL,
+  `session_device` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `session_ip` varchar(255) CHARACTER SET utf8 NOT NULL,
   `session_valid` int(11) NOT NULL DEFAULT 0,
   `last_online` int(11) NOT NULL DEFAULT 0,
-  `browser_id` varchar(32) NOT NULL,
+  `browser_id` varchar(32) CHARACTER SET utf8 NOT NULL,
   `login_time` int(11) NOT NULL,
   PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `twofactorauth` (
   `auth_key` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   KEY `auth_key` (`auth_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `active` int(1) NOT NULL DEFAULT 0,
-  `user_firstname` varchar(20) NOT NULL,
-  `user_lastname` varchar(20) NOT NULL,
-  `user_nickname` varchar(20) DEFAULT NULL,
-  `user_password` varchar(255) NOT NULL,
-  `user_email` varchar(255) NOT NULL,
-  `user_gender` char(1) NOT NULL,
+  `user_firstname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_lastname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_nickname` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `user_password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `user_email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `user_gender` char(1) CHARACTER SET utf8 NOT NULL,
   `user_birthdate` int(11) NOT NULL,
   `user_create_date` int(11) NOT NULL,
-  `user_status` char(1) DEFAULT 'N',
-  `user_about` text NOT NULL,
-  `user_hometown` varchar(255) NOT NULL,
-  `user_token` varchar(255) NOT NULL,
+  `user_status` char(1) CHARACTER SET utf8 DEFAULT 'N',
+  `user_about` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_hometown` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `user_token` varchar(255) CHARACTER SET utf8 NOT NULL,
   `pfp_media_id` int(11) NOT NULL,
   `cover_media_id` int(11) NOT NULL,
   `verified` int(1) NOT NULL DEFAULT 0,
@@ -102,5 +103,5 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_email` (`user_email`),
   UNIQUE KEY `user_nickname` (`user_nickname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
